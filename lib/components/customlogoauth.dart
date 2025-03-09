a5292d3e87b37566961b6f1ae8c38c7f05303eb0
abdellahcodes <ayoujil.abdellah3@gmail.com>
2025-03-09 22:17:53 +0000

    Initial commit of Flutter project

---
+++ lib/components/customlogoauth.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class CustomLogoAuth extends StatelessWidget {
+  const CustomLogoAuth({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+                child: Container(
+                    alignment: Alignment.center,
+                    width: 120,
+                    height: 120,
+                    padding: const EdgeInsets.all(1),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(70)),
+                    child: Lottie.asset(
+                    "images/notelottie.json", 
+                    width: 100,
+                  )),
+              ); 
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+
+import 'package:firebase_core/firebase_core.dart';
+import 'auth/login.dart';
+import 'auth/signup.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+      routes: {
+        "signup" : (context) => SignUp() , 
+        "login" : (context) => Login()
+      },
+    );
+  }
+}
