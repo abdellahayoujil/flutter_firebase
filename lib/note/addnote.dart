6b0f99b6976d7f98740ae9301378ae00e099a170
abdellahcodes <ayoujil.abdellah3@gmail.com>
2025-03-14 17:28:25 +0000

    add data to Subcollections

---
+++ lib/note/addnote.dart
@@ -0,0 +1,86 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_flutter/components/custombuttonauth.dart';
+import 'package:firebase_flutter/components/customformfield.dart';
+import 'package:firebase_flutter/note/viewnote.dart';
+import 'package:flutter/material.dart';
+
+class Addnote extends StatefulWidget {
+  final String docId;
+  const Addnote({super.key, required this.docId});
+
+  @override
+  State<Addnote> createState() => _AddnoteState();
+}
+
+class _AddnoteState extends State<Addnote> {
+  TextEditingController note = TextEditingController();
+
+  bool isLoading = false;
+
+  Future<void> addNote() async {
+    CollectionReference collectionnote = FirebaseFirestore.instance
+        .collection('categoris')
+        .doc(widget.docId)
+        .collection("note");
+    if (formstate.currentState!.validate()) {
+      try {
+        isLoading = true;
+        setState(() {});
+        // ignore: unused_local_variable
+        DocumentReference res = await collectionnote.add({'note': note.text});
+        // ignore: use_build_context_synchronously
+        Navigator.of(context).push(
+            MaterialPageRoute(
+                builder: (context) => Viewpage(categoryId: widget.docId)));
+      } catch (e) {
+        isLoading = false;
+        setState(() {});
+        // ignore: avoid_print
+        print("error ================= $e");
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    note.dispose();
+  }
+
+  GlobalKey<FormState> formstate = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Add Note")),
+      body: Form(
+          key: formstate,
+          child: isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20, horizontal: 25),
+                      child: CustomformfieldAdd(
+                          hinttext: "Enter your note",
+                          mycontroller: note,
+                          validator: (val) {
+                            if (val == "") {
+                              return "can't to be empty";
+                            }
+                            return null;
+                          }),
+                    ),
+                    CustomButtonAuth(
+                      title: "Add",
+                      onPressed: () {
+                        addNote();
+                      },
+                    )
+                  ],
+                )),
+    );
+  }
+}
--- lib/note/viewnote.dart
+++ lib/note/viewnote.dart
@@ -3,6 +3,7 @@ import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_flutter/constans.dart';
+import 'package:firebase_flutter/note/addnote.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -22,7 +23,9 @@ class _ViewpageState extends State<Viewpage> {
   getData() async {
     QuerySnapshot querySnapshot = await FirebaseFirestore.instance
         .collection('categoris')
-        .doc(widget.categoryId).collection("note").get();
+        .doc(widget.categoryId)
+        .collection("note")
+        .get();
     data.addAll(querySnapshot.docs);
     isLoading = false;
     setState(() {});
@@ -40,7 +43,8 @@ class _ViewpageState extends State<Viewpage> {
         floatingActionButton: FloatingActionButton(
           backgroundColor: MyColors.myYellow,
           onPressed: () {
-            Navigator.of(context).pushNamed("addcategory");
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => Addnote(docId: widget.categoryId)));
           },
           child: const Icon(Icons.add),
         ),
@@ -59,75 +63,58 @@ class _ViewpageState extends State<Viewpage> {
                 icon: const Icon(Icons.logout))
           ],
         ),
-        body: isLoading
-            ? const Center(child: Text("Loading ..."))
-            : GridView.builder(
-                itemCount: data.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2, mainAxisExtent: 160),
-                itemBuilder: (BuildContext context, int index) {
-                  return InkWell(
-                    onLongPress: () {
-                      AwesomeDialog(
-                          // ignore: use_build_context_synchronously
-                          context: context,
-                          dialogType: DialogType.warning,
-                          animType: AnimType.rightSlide,
-                          title: 'Error',
-                          desc: 'what do you want ! ',
-                          btnCancelText: "delete",
-                          btnOkText: "update",
-                          btnCancelOnPress: () async {
-                            /*await FirebaseFirestore.instance
+        // ignore: deprecated_member_use
+        body: WillPopScope(
+            child: isLoading
+                ? const Center(child: Text("Loading ..."))
+                : GridView.builder(
+                    itemCount: data.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2, mainAxisExtent: 160),
+                    itemBuilder: (BuildContext context, int index) {
+                      return InkWell(
+                        onLongPress: () {
+                          AwesomeDialog(
+                                  // ignore: use_build_context_synchronously
+                                  context: context,
+                                  dialogType: DialogType.warning,
+                                  animType: AnimType.rightSlide,
+                                  title: 'Error',
+                                  desc: 'what do you want ! ',
+                                  btnCancelText: "delete",
+                                  btnOkText: "update",
+                                  btnCancelOnPress: () async {
+                                    /*await FirebaseFirestore.instance
                                 .collection('categoris')
                                 .doc(data[index].id)
                                 .delete();
                             // ignore: use_build_context_synchronously
                             Navigator.of(context)
                                 .pushReplacementNamed("homepage");*/
-                          },
-                          btnOkOnPress: () async {
-                           /* Navigator.of(context).push(MaterialPageRoute(
+                                  },
+                                  btnOkOnPress: () async {
+                                    /* Navigator.of(context).push(MaterialPageRoute(
                                 builder: (context) => Updatecategoris(
                                     docid: data[index].id, oldname : data[index]['name'])));*/
-                          }).show();
-                    },
-                    child: Card(
-                      child: Container(
-                        padding: const EdgeInsets.all(15),
-                        child: Column(
-                          children: [
-                            Text("${data[index]['note']}")
-                          ],
+                                  })
+                              .show();
+                        },
+                        child: Card(
+                          child: Container(
+                            padding: const EdgeInsets.all(20),
+                            child: Column(
+                              children: [Text("${data[index]['note']}")],
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                  );
-                },
-
-                /*FirebaseAuth.instance.currentUser!.emailVerified
-            ? const Text("succesfuly verifed")
-            : MaterialButton(
-              textColor: Colors.white,
-              color: Colors.red,
-              onPressed: (){
-                FirebaseAuth.instance.currentUser!.sendEmailVerification();
-            },child: const Text("please verifed Email"),)
-
-            Card(
-              child: Container(
-                padding: EdgeInsets.all(15),
-                child: Column(
-                  children: [
-                    Image.asset(
-                      "images/fl.png",
-                      height: 100,
-                    ),
-                    Text("Favorit")
-                  ],
-                ),
-              ),
-            )*/
-              ));
+                      );
+                    },
+                  ),
+            onWillPop: () {
+              Navigator.of(context)
+                  .pushNamedAndRemoveUntil("homepage", (route) => false);
+              return Future.value(false);
+            }));
   }
 }
